0fa50fdb8ed1043f4752df24e41a71ef39c7eb2e
Luis Argumedo <huicho.argumedo@gmail.com>
2015-01-06 19:33:04 -0600

    annotated code for docco

--- app.rb
+++ app.rb
@@ -1,91 +1,69 @@
+# Configuración inicial
+# ### Dependencias
+# Sinatra para nuestro servidor web
 require 'sinatra'
+# Conector de mongoDB para la base de datos
 require 'mongo'
+# Motor de plantillas para enviar las vistas
 require 'slim'
+# JSON para enviar respuestas asíncronas en este formato
 require 'json/ext'
 
 include Mongo
 
+# ### Configuración
 configure do
+# Conexión a la base de datos
   conn = MongoClient.new("localhost", 27017)
   set :mongo_connection, conn
+# Variable donde tendremos nuestra conexión
   set :mongo_db, conn.db("conafor")
 end
 
+# ### Rutas
+
+# Ruta de inicio donde se carga nuestra
+# plantilla con angularJS, ya que la lógica se
+# ejecuta del lado del cliente
 get '/' do
   slim :index
 end
 
+# En esta ruta obtenemos los templates de slim
+# para enviar el html cuando la ruta de angular 
+# así lo necesite
 get '/templates/*.html' do |name|
   slim name.to_sym, layout: false
 end
 
+# Con esta ruta envíamos un archivo para descargar
+# desde `/public/files`. Esta se usa para poder descargar
+# las fichas en esa sección
 get '/download/:name' do |name|
   send_file "./public/files/#{name}", filename: name, type: "Application/octet-stream"
 end
 
-get '/hello' do
-  "Hello World!"
-end
-
+# En esta ruta obtenemos los posibles valores para el campo
+# que halla sido enviado como parámetro haciendo un *distinct*
+# en la colección
 get '/get_domain/:field' do
   settings.mongo_db['listados'] 
   .distinct(params[:field]).sort.to_json
 end
 
-get '/cantidad_solicitado/*' do |year|
-
-  query = [ 
-    { "$group" => {
-      _id: { 
-        code: "$code",
-        region: "$region"
-      },
-      cantidad: {
-        "$sum" => 1
-      }
-    }},
-    { "$sort" => {
-      cantidad: 1
-    }}
-  ]
-
-  unless year.nil?
-   query.unshift({ "$match" => {
-      fecha: params[:year]
-    }})
-  end
-
-  settings.mongo_db['listados'] 
-  .aggregate(query).to_a.to_json
-
-end
-
-get '/cantidad_solicitado/summary/:code' do
-  p params[:code]
-  settings.mongo_db['listados'] 
-  .aggregate([ 
-    { "$match" => {
-      region: params[:code].to_i
-    }},
-    { "$group" => {
-      _id: {
-        region: "$code"
-      },
-      total: {
-        "$sum" => 1
-      }
-    }}
-  ]).to_a.first.to_json
-end
-
+# Aquí podemos obtener la ficha de resumen del monto solicitado
+# para la región seleccionado con la posibilidad de agregar un filtro
 get '/total_solicitado/summary/:code/*.*' do
   name = params[:splat].first
   value = params[:splat].last
 
+# Pasamos el parámetro *:code* para filtrar por esa región
   query = [ 
     { "$match" => {
       region: params[:code].to_i
     }},
+# Agrupamos en base a la región y a la fecha para tener
+# los datos separados según la fecha en cada una de las regiones
     { "$group" => {
       _id: { 
         region: "$region",
@@ -114,6 +92,8 @@ get '/total_solicitado/summary/:code/*.*' do
     }}
   ]
 
+# Si tenemos filtros que agregar añadimos esa sentencia *$match*
+# al inicio de nuestra consulta para hacer el filtrado 
   unless name == 'none'
     value  = value.to_i == 0? value : value.to_i
     query.unshift({ "$match" => {
@@ -125,6 +105,8 @@ get '/total_solicitado/summary/:code/*.*' do
   .aggregate(query).to_a.to_json
 end
 
+# Similar al resumen pero obtenemos las métricas para todas las regiones
+# y no solo la que hallamos seleccionado
 get '/total_solicitado/*.*' do
 
   name = params[:splat].first
@@ -163,6 +145,8 @@ get '/total_solicitado/*.*' do
   .aggregate(query).to_a.to_json
 end
 
+# En esta ruta vamos a obtener los valores agrupados de cada región
+# con su código de estado
 get '/region_by_code' do
   content_type :json
 
@@ -179,6 +163,8 @@ get '/region_by_code' do
   .aggregate(query).to_a.to_json
 end
 
+# Aquí agrupamos las cantidades de montos según el campo
+# que halla sido seleccionado en nuestro parametro *:filter*
 get '/by/:filter' do |filter|
   content_type :json
 
@@ -190,7 +176,7 @@ get '/by/:filter' do |filter|
       sum: { "$sum" =>  1 }
     }},
     { "$sort" => {
-      "_id.filter":  1
+      "_id.filter" =>  1
     }}]
 
   settings.mongo_db['listados'] 
@@ -213,12 +199,13 @@ get '/grupo_apoyo_region' do
   .aggregate(query).to_a.to_json
 end
 
-get '/:estado' do
+# Todos los montos solicitados para la región seleccionada
+get '/:region' do
   content_type :json
 
   query = {}
-  query[:estado] = params[:estado]
+  query[:region] = params[:region]
   p query
 
-  settings.mongo_db['listados'].find(query).limit(100).to_a.to_json
+  settings.mongo_db['listados'].find(query).to_a.to_json
 end
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,22 @@
+// General
+// ------------------------
+
+// Tenemos todas las rutas y controladores de angularJS en este archivo.
+// Aquí es donde se encuentra toda la lógica de la aplicación del lado del cliente a 
+// excepción de las directivas que están en su propio archivo.
+
+// Misc
+// -----------------------
+
+// Esta función sirve para separar por comas las cantidades con tres o más dígitos.
+// `Parámetro entero x`
+// `Retorna x`
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+// Aquí se crea una **Factory** de angularJS para poder inyectar la librería 
+// d3 a nuestro módulo 
 angular.module('d3', [])
   .factory('d3Service', [function() {
     var d3;
@@ -8,29 +24,40 @@ angular.module('d3', [])
     return d3;
   }]);
 
+// Declaramos nuestro módulo con la lista de dependencias a inyectar
 var conaforApp = angular.module('conaforApp', ['ngRoute', 'd3']);
 
+// Gestión de rutas
+// --------------------
+
+// En esta parte declaramos las rutas para para nuestra aplicación que
+// serán gestionadas totalmente por el lado del cliente con el módulo ngRoute.
+// Cada ruta recibe como parametro que template y que controlador usará
 conaforApp.config(function($routeProvider) {
   $routeProvider
     .when('/graph', {
       templateUrl: '/templates/graph.html',
       controller: 'graphCtrl'
     })
-    .when('/eje1', {
+    .when('/map', {
       templateUrl: '/templates/map.html',
-      controller: 'eje1Ctrl'
+      controller: 'mapCtrl'
     })
     .when('/fichas', {
       templateUrl: '/templates/fichas.html',
       controller: 'fichasCtrl'
     })
-    .when('/', {
-      templateUrl: '/templates/home.html',
-      controller: 'aboutCtrl'
-    });
 });
 
+// Controladores
+// --------------------
+
+// ### graphController
+// El controlador *graphCtrl* contiene lo necesario para pedir los datos 
+// usados para crear las gráficas
 conaforApp.controller('graphCtrl', function($scope, $http) {
+  // Esta función obtiene la cantidad de montos solicitados agrupados por
+  // el campo seleccionado de manera asíncrona
   $scope.cantidad = function(filter, name) {
     filter = filter || 'none';
 
@@ -41,84 +68,80 @@ conaforApp.controller('graphCtrl', function($scope, $http) {
       $scope.data = data;
     })
     .error(function(data, status, headers, config) {
-      console.log('DAMN');
+      console.log('Error al obtener los datos');
     });
   };
+  // La primera vez que entremos agrupar por región
   $scope.cantidad('region_name');
 });
 
-conaforApp.controller('aboutCtrl', function($scope, $http) {
-  $http.get('/JALISCO')
-  .success(function(data, status, headers, config) {
-    $scope.entries = data;
-  }).error(function(data, status, headers, config) {
-    // yolo
-  });
-});
-
+// ### fichasCtrl
+// El controlador *fichasCtrl* no contiene lógica, aunque puede agregarse
+// el funcionamiento necesario para la vista de las fichas en este controlador
+// cuando sea necesario
 conaforApp.controller('fichasCtrl', function($scope, $http) { 
 });
 
-conaforApp.controller('eje1Ctrl', function($scope, $http) { 
-  $scope.estado = 'Selecciona un región';
+// ### mapCtrl
+// Este controlador contiene todo lo necesario para filtrar la información en el mapa
+// así como lo necesario para su funcionamiento
+conaforApp.controller('mapCtrl', function($scope, $http) { 
+  // Función para reiniciar la lista de resultados y el título de la ficha de resumen
   function reset() {
       $scope.estado = 'Selecciona una región';
       $scope.results = {};
   }
-  $scope.message = 'Total Montos Solicitados';
+
+  // Valores por defecto para los títulos
+  $scope.estado = 'Selecciona un región';
+  $scope.message = 'Montos Solicitados';
   $scope.date = '2009 - 2013';
+
+  // #### Filtrado
+  // Cada petición obtiene el dominio (posibles valores) para
+  // cada uno de los filtros
+  
+  // Filtro para los tipos de apoyo
   $http.get('/get_domain/grupo_tipo_apoyo')
   .success(function(data, status, headers, config) {
     $scope.grupos = data;
   })
+  // Filtro para los tipos de persona solicitante
   $http.get('/get_domain/tipo_solicitante')
   .success(function(data, status, headers, config) {
     $scope.personas = data;
   })
   .error(function(data, status, headers, config) {
-    console.log('DAMN');
+    console.log('Error al obtener los datos');
   });
+  // Tipo de solicitante (Ejido, comunidad, etc)
   $http.get('/get_domain/tipo')
   .success(function(data, status, headers, config) {
     $scope.tipos = data;
   })
   .error(function(data, status, headers, config) {
-    console.log('DAMN');
+    console.log('Error al obtener los datos');
   });
 
-  // WARNING!!
-  // SOLO CAMPOS NUMERICOS 
   $scope.results = {};
 
-  $scope.count = function() {
-    reset();
-    $scope.fields = [
-      { name: 'total', show: 'Total' }
-    ];
-    $scope.url = '/cantidad_solicitado';
-    $scope.field = 'cantidad';
-    
-    $http.get('/cantidad_solicitado')
-    .success(function(data, status, headers, config) {
-      $scope.data = data;
-      /*$timeout(function() {
-        angular.element('#year-filter').trigger('click');
-      });*/
-    })
-    .error(function(data, status, headers, config) {
-      console.log('DAMN');
-    });
-  };
-
+  // #### Tratamiento de datos
+  // En esta función se obtiene los datos de cantidad, promedio y total
+  // gastado en montos solicitados a lo largo de todos los años filtrados
+  // por el campo seleccionado
   $scope.monto = function(name, val) {
     reset();
+    // Los campos que se van a recibir y como se muestran en el resumen
     $scope.fields = [
       { name: 'cantidad', show: 'Cantidad' },
       { name: 'promedio', show: 'Promedio (MXN)' },
       { name: 'total', show: 'Total (MXN)' }
     ];
+    // La ruta donde se buscara
     $scope.url = '/total_solicitado';
+    // El campo usado para comparar en el mapa
     $scope.field = 'cantidad';
+    // Valores por defecto para el filtro
     name = name || 'none';
     val = val || 'none';
 
@@ -133,19 +156,26 @@ conaforApp.controller('eje1Ctrl', function($scope, $http) {
       $scope.data = data;
     })
     .error(function(data, status, headers, config) {
-      console.log('DAMN');
+      console.log('Error al obtener los datos');
     });
   };
 
+  // #### Funcionamiento al momento de dar click a una región
   $scope.onClick = function(item) {
+    // Es necesario porque d3 funciona fuera del scope de angular
     $scope.$apply(function() {
 
-      console.log($scope.filter, $scope.filter_val);
+      // Pedimos el resumen de esa región según el filtro seleccionado
       $http.get($scope.url + '/summary/' + item.region + '/' + $scope.filter + '.' + $scope.filter_val)
       .success(function(data, status, headers, config) {
         $scope.results = {};
         $scope.estado = 'Región ' + item.name;
           
+        // Iteramos los anteriores campos que se definieron en la función que pedirá
+        // los datos para el mapa para ponerlos en la ficha de resumen.
+        // Recorremos cada año y después por cada campo ponemos el valor correspondiente
+        // y agregamos comas si es necesario
+        // **Solo funciona con valores númericos**
         angular.forEach(data, function(v, k) {
           $scope.results[v.fecha] = [];
           angular.forEach($scope.fields, function(field, key) {
@@ -162,13 +192,12 @@ conaforApp.controller('eje1Ctrl', function($scope, $http) {
           });
         });
         console.log($scope.results);
-        /*$scope.avg = numberWithCommas('$' + data.avg.toFixed());
-        $scope.total = numberWithCommas('$' + data.total.toFixed());*/
       })
       .error(function(data, status, headers, config) {
         $scope.response = 'Error al cargar';
       });
     });
   };
+  // La primera vez cargaremos el mapa sin filtros
   $scope.monto();
 });
